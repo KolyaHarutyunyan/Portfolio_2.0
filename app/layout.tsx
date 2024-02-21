493192b500b92e13cc390ffbc09075e87f01ffd8
Kolya <51478477+KolyaHarutyunyan@users.noreply.github.com>
2024-02-21 17:00:58 +0400

    feat: added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,84 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { PublicLinks, siteConfig } from "@/configs/site";
+// import { PublicLinks, siteConfig } from "@/configs/site";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const host = PublicLinks.ROOT;
+// const host = PublicLinks.ROOT;
+
+// export const metadata: Metadata = {
+//   metadataBase: new URL(host),
+//   title: {
+//     default: siteConfig.name,
+//     template: `%s | ${siteConfig.name}`,
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     "max-snippet": -1,
+//     "max-image-preview": "large",
+//     "max-video-preview": -1,
+//   },
+//   description: siteConfig.description,
+//   keywords: siteConfig.keywords,
+//   openGraph: {
+//     type: "website",
+//     locale: "en_US",
+//     images: siteConfig.ogImage,
+//     url: siteConfig.url,
+//     title: siteConfig.name,
+//     description: siteConfig.description,
+//     siteName: siteConfig.name,
+//   },
+//   twitter: {
+//     images: siteConfig.twitter?.images,
+//     card: "summary_large_image",
+//     creator: "@KolyaHarutyunyan",
+//     site: "@kolyasportfoliocom",
+//     title: siteConfig.name,
+//     description: siteConfig.description,
+//   },
+//   icons: {
+//     icon: "/favicon.ico",
+//     shortcut: "/favicon-16x16.png",
+//     apple: "/apple-touch-icon.png",
+//   },
+//   manifest: `${siteConfig.url}/site.webmanifest`,
+// };
 
 export const metadata: Metadata = {
-  metadataBase: new URL(host),
+  metadataBase: new URL("https://kolya-s-portfolio.com/"),
   title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
+    default: "Kolya's Portfolio",
+    template: `%s | "Kolya's Portfolio"`,
   },
-  robots: {
-    index: true,
-    follow: true,
-    "max-snippet": -1,
-    "max-image-preview": "large",
-    "max-video-preview": -1,
-  },
-  description: siteConfig.description,
-  keywords: siteConfig.keywords,
+  description:
+    "I have got just what you need. Let's build the future together!",
   openGraph: {
     type: "website",
     locale: "en_US",
-    images: siteConfig.ogImage,
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
+    images: "/images/social/og.png",
+    url: "https://kolya-s-portfolio.com/",
+    title: "Kolya's Portfolio",
+    description:
+      "I have got just what you need. Let's build the future together!",
+    siteName: "Kolya's Portfolio",
   },
   twitter: {
-    images: siteConfig.twitter?.images,
+    images: "/images/social/og.png",
     card: "summary_large_image",
     creator: "@KolyaHarutyunyan",
     site: "@kolyasportfoliocom",
-    title: siteConfig.name,
-    description: siteConfig.description,
+    title: "Kolya's Portfolio",
+    description:
+      "I have got just what you need. Let's build the future together!",
   },
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-};
-
-const jsonLd = {
-  "@context": "https://schema.org",
-  "@type": "WebPage",
-  name: siteConfig.name,
-  image: siteConfig.ogImage,
-  description: siteConfig.description,
+  manifest: `https://kolya-s-portfolio.com/site.webmanifest`,
 };
 
 export default function RootLayout({
@@ -61,12 +88,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
