75853a39a6775e889f3ffadf2a41d18e3eee38cb
Kolya <51478477+KolyaHarutyunyan@users.noreply.github.com>
2024-02-24 16:23:48 +0400

    fix: metadata for website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,43 @@
-// import type { Metadata } from "next";
-import Head from "next/head";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   metadataBase: new URL("https://kolya-s-portfolio.com/"),
-//   title: {
-//     default: "Kolya's Portfolio",
-//     template: `%s | "Kolya's Portfolio"`,
-//   },
-//   description:
-//     "I have got just what you need. Let's build the future together!",
-//   openGraph: {
-//     type: "website",
-//     locale: "en_US",
-//     images: "/images/social/og.png",
-//     url: "https://kolya-s-portfolio.com/",
-//     title: "Kolya's Portfolio",
-//     description:
-//       "I have got just what you need. Let's build the future together!",
-//     siteName: "Kolya's Portfolio",
-//   },
-//   twitter: {
-//     images: "/images/social/og.png",
-//     card: "summary_large_image",
-//     creator: "@KolyaHarutyunyan",
-//     site: "@kolyasportfoliocom",
-//     title: "Kolya's Portfolio",
-//     description:
-//       "I have got just what you need. Let's build the future together!",
-//   },
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-16x16.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-//   manifest: `https://kolya-s-portfolio.com/site.webmanifest`,
-// };
+export const metadata: Metadata = {
+  metadataBase: new URL("https://kolya-s-portfolio.com/"),
+  title: {
+    default: "Kolya's Portfolio",
+    template: `%s | "Kolya's Portfolio"`,
+  },
+  description:
+    "I have got just what you need. Let's build the future together!",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    images: "/images/social/og.png",
+    url: "https://kolya-s-portfolio.com/",
+    title: "Kolya's Portfolio",
+    description:
+      "I have got just what you need. Let's build the future together!",
+    siteName: "Kolya's Portfolio",
+  },
+  twitter: {
+    images: "/images/social/og.png",
+    card: "summary_large_image",
+    creator: "@KolyaHarutyunyan",
+    site: "@kolyasportfoliocom",
+    title: "Kolya's Portfolio",
+    description:
+      "I have got just what you need. Let's build the future together!",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `https://kolya-s-portfolio.com/site.webmanifest`,
+};
 
 // export const metadata: Metadata = {
 //   title: "Kolya's Portfolio",
@@ -53,27 +52,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <title>{"Kolya's Portfolio"}</title>
-        <meta
-          name="description"
-          content={
-            "I have got just what you need. Let's build the future together!"
-          }
-        />
-        <meta property="og:title" content={"Kolya's Portfolio"} />
-        <meta
-          property="og:description"
-          content={
-            "I have got just what you need. Let's build the future together!"
-          }
-        />
-        <meta property="og:image" content="/images/social/og.png" />
-        <meta property="og:image:width" content="500" />
-        <meta property="og:image:height" content="500" />
-        <meta property="og:url" content="https://kolya-s-portfolio.com/" />
-        <meta property="og:siteName" content="Kolya's Portfolio" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
