4a98abb90f37ea0ad219111758a0a7162051f763
Kolya <51478477+KolyaHarutyunyan@users.noreply.github.com>
2024-02-24 18:12:39 +0400

    fix: changed metadata for page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { metaSocialGenerator } from "@/utils";
 import { siteConfig } from "@/configs/site";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
@@ -17,10 +16,53 @@ import {
   getSocials,
 } from "../api";
 
-export const metadata: Metadata = metaSocialGenerator({
-  title: "Kolya's Portfolio",
-  desc: "I have got just what you need. Let's build the future together!",
-});
+const host = siteConfig.url;
+
+export const metadata: Metadata = {
+  metadataBase: new URL(host),
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-snippet": -1,
+    "max-image-preview": "large",
+    "max-video-preview": -1,
+  },
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  alternates: {
+    canonical: "./",
+    types: {
+      "application/rss+xml": `${siteConfig.url}/feed.xml`,
+    },
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    images: siteConfig.ogImage,
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    images: siteConfig.twitter.images,
+    card: "summary_large_image",
+    creator: siteConfig.twitter.creator,
+    site: siteConfig.twitter.site,
+    title: siteConfig.name,
+    description: siteConfig.description,
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
 
 export default async function Home() {
   const socials = await getSocials();
